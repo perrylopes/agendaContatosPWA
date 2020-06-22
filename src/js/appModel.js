c6ff80e0cfd4339d0e55a113f893ad62fb4b106b
Dev Goseller <development.goseller@gmail.com>
2020-06-22 10:38:08 -0300

    Acrescentando na Url do Ajax o protocologo HTTPS

--- src/js/appModel.js
+++ src/js/appModel.js
@@ -1,17 +1,16 @@
-function contatos($, listaContatos){
+function contatos($, listaContatos) {
   $.ajax({
-        url: "http://www.mocky.io/v2/5bdf4f3a310000ea149e4009",
-        method: "get",
-        dataType: "json",
-        success: contatos => { 
-           localStorage.setItem('contatos', JSON.stringify(contatos.contatos))
-           listaContatos(contato = JSON.parse(localStorage.getItem('contatos')));
-          },
-           error: function() {
-            listaContatos(contato = JSON.parse(localStorage.getItem('contatos')));   
-        },
-      });
-   
+    url: "https://www.mocky.io/v2/5bdf4f3a310000ea149e4009",
+    method: "get",
+    dataType: "json",
+    success: (contatos) => {
+      localStorage.setItem("contatos", JSON.stringify(contatos.contatos));
+      listaContatos((contato = JSON.parse(localStorage.getItem("contatos"))));
+    },
+    error: function () {
+      listaContatos((contato = JSON.parse(localStorage.getItem("contatos"))));
+    },
+  });
 }
 
-module.exports = {contatos}
+module.exports = { contatos };
